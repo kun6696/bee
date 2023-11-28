20ac0471f3cd4684a7a1aa5715a997a532c843d4
xionghe.hx <xionghe.hx@digital-engine.com>
2023-11-28 19:03:52 +0800

    chore: init config

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'import/prefer-default-export': 0,
+    'no-use-before-define': 0,
+    'no-shadow': 0,
+    'no-restricted-syntax': 0,
+    'no-return-assign': 0,
+    'no-param-reassign': 0,
+    'no-sequences': 0,
+    'no-loop-func': 0,
+    'no-nested-ternary': 0,
+  },
+};
